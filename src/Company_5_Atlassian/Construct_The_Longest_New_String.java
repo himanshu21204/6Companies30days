63598aad87be0d3aca210246b36d9df256d23a38
himanshuparmar21 <himanshu.parmar21204@gmail.com>
2025-01-13 14:29:30 +0530

    Day 13

---
+++ src/Company_5_Atlassian/Construct_The_Longest_New_String.java
@@ -0,0 +1,18 @@
+package Company_5_Atlassian;
+// https://leetcode.com/problems/construct-the-longest-new-string/
+public class Construct_The_Longest_New_String {
+    public int longestString(int x, int y, int z) {
+        // Find the minimum of x and y
+        int min = Math.min(x, y);
+
+        // If x and y are equal, calculate the result using a specific formula
+        if (x == y) {
+            // When x equals y, the result is (min * 2 + z) * 2
+            return (min * 2 + z) * 2;
+        }
+
+        // If x is not equal to y, calculate the result using another formula
+        // Here, we add 1 to the sum of min * 2 and z before multiplying by 2
+        return (min * 2 + 1 + z) * 2;
+    }
+}
