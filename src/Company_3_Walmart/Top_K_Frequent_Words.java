d41113c4221ae90a441154c85da59b84940ccaac
himanshuparmar21 <himanshu.parmar21204@gmail.com>
2025-01-18 23:42:16 +0530

    Day 18

---
+++ src/Company_3_Walmart/Top_K_Frequent_Words.java
@@ -0,0 +1,26 @@
+package Company_3_Walmart;
+import java.util.*;
+// https://leetcode.com/problems/top-k-frequent-words/description/
+public class Top_K_Frequent_Words {
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String,Integer> map = new HashMap<>();
+        for(String word : words){
+            map.put(word,map.getOrDefault(word,0)+1);
+        }
+
+        Queue<String> pq = new PriorityQueue<>((w1,w2)->map.get(w1).equals(map.get(w2))?w2.compareTo(w1):map.get(w1)-map.get(w2));
+
+        for(String key : map.keySet()){
+            pq.offer(key);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        List<String> list = new ArrayList<>();
+        while (!pq.isEmpty()){
+            list.add(pq.poll());
+        }
+        Collections.reverse(list);
+        return list;
+    }
+}
