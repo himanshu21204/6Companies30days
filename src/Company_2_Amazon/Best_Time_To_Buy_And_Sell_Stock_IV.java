a50edd93639f704d6c908b48b68ed01d33be65f2
himanshuparmar21 <himanshu.parmar21204@gmail.com>
2025-01-26 00:03:36 +0530

    Day 25

---
+++ src/Company_2_Amazon/Best_Time_To_Buy_And_Sell_Stock_IV.java
@@ -0,0 +1,23 @@
+package Company_2_Amazon;
+
+public class Best_Time_To_Buy_And_Sell_Stock_IV {
+    public static int maxProfit(int K, int[] prices) {
+        int cap = K;
+        int n = prices.length;
+        int[][] after = new int[2][cap + 1];
+        int[][] curr = new int[2][cap + 1];
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int k = 1; k <= cap; k++) {
+                    if (buy == 1) {
+                        curr[buy][k] = Math.max(-prices[idx] + after[0][k], after[1][k]);
+                    } else {
+                        curr[buy][k] = Math.max(prices[idx] + after[1][k - 1], after[0][k]);
+                    }
+                }
+            }
+            after = curr;
+        }
+        return after[1][cap];
+    }
+}
